eac995640b1148f6f6017cb9fd9973ec7570b16f
Lukas Wolfsberger <1245519+wolfsblu@users.noreply.github.com>
2026-01-10 16:21:48 +0100

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -13,11 +13,6 @@ export default defineConfig({
 		sveltekit(),
 		paraglideVitePlugin({ project: './project.inlang', outdir: './src/lib/paraglide' })
 	],
-	server: {
-		fs: {
-			allow: ['..']
-		}
-	},
 	ssr: {
 		noExternal: ['@cooklang/cooklang']
 	},
